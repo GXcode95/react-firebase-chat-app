9574858b6f7f2ef3c2ef225126ab8e55b4dd8342
GXcode95 <xavier.grenouillet@gmail.com>
2022-02-02 16:54:20 +0100

    done: hearth animation

--- src/components/BackgroundAnimation/Hearth.jsx
+++ src/components/BackgroundAnimation/Hearth.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, Grid} from '@mui/material'
 
 import './hearth.scss'
 
 const Hearth = () => {
   
+
+  const randomIntFromInterval = (min, max) => { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
   
   return ( 
     <>
-    <Box className="hearth-wrapper" position="absolute" sx={{top: "300px", right:0}}>
-      <div className="hearth"></div>
-    </Box>
-
-    <Box className="hearth-wrapper" position="absolute" sx={{top: "300px", left: "70px"}}>
-      <div className="hearth"></div>
-    </Box>
+    <Grid container className="hearth-grid" rowSpacing={10}>
+      {new Array(24).fill(0).map( entry => 
+        
+        <Grid item xs={3}>
+          <Box className={`hearth-wrapper n${randomIntFromInterval(1,15)}`} position="relative" >
+            <div className='hearth'></div>
+          </Box>
+        </Grid>
+      )}
+    </Grid>
 
     
       
